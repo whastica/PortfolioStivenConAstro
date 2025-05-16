2fdd085ab53002801c1535c37f56ebf911b1d968
whastica <drlook92@gmail.com>
2025-05-16 00:31:44 -0500

    Cambio presentacion de los proyectos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,13 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+	  extend: {
+		colors: {
+		  'custom-cyan': '#00BCD4',
+		  'custom-orange': '#FFA726',
+		  'custom-pink': '#E91E63',
+		},
+	  },
 	},
 	plugins: [],
-}
+  }
